425eace48c6353a7ed8839348a73ed716dd54180
chendahui007 <chendahui007@gmail.com>
2012-10-30 17:24:31 +0800

    update

--- display.sh
+++ display.sh
@@ -3,7 +3,10 @@
 CURR_STATE=`xrandr -q |grep VGA-0 | awk '{print $3}' | wc -m`
 #echo $CURR_STATE
 if [ $CURR_STATE = '16'  ]
-    then `xrandr --output VGA-0 --off`
-    else `xrandr --output VGA-0  --mode 1024x768 --right-of LVDS`
-
+    then 
+       eval  `xrandr --output VGA-0 --off`
+       eval  `cat ~/.fehbg`
+    else
+      eval  `xrandr --output LVDS --mode 1366x768 --output VGA-0 --mode 1024x768 --right-of LVDS `
+      eval  `nitrogen --restore`
 fi
