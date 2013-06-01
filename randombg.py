9c25c871caaab099dceb1290fcb9481102497eb1
holys <chendahui007@gmail.com>
2013-06-01 23:27:47 +0800

    auto-switch between home and external monitor

--- randombg.py
+++ randombg.py
@@ -1,20 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/python 
 # -*- coding: utf8 -*-
+"""  
+randombg.py
+by holys <chendahui007@gmail.com>
+
+= simple description =
+
+Simple script to change background pictures randomly and regularly
+Require setting up your crontab:
+
+* * * * * /path/to/this_script.py > /path/to/logfile 2>&1
+Set up above to suit your needs
+Don't forget to change the access mode of the script to `a+x` or others
+
+"""
+
 
 import random
 import subprocess
 import os 
-import time 
-
-
-
-
+    
+    
 BACKGROUND = '/home/david/wallpaper/dell2312'
 IGNORE_FILES = ['/home/david/wallpaper/dell2312/.directory']
-#TIME=10
-
-
-def enumerate():
+    
+    
+def enumerate():                                                   
     global BACKGROUND
     file_collections = []
     for root, dirs, files in os.walk(BACKGROUND):
@@ -25,17 +36,15 @@ def enumerate():
 
 def randombg():
     select_files = list(set(enumerate())-set(IGNORE_FILES))
-    subprocess.call(['feh', '--bg-scale', random.choice(select_files)])
+    #subprocess.call(['feh', '--bg-scale', random.choice(select_files)])
+    # make it run in cron job
+    subprocess.call('export DISPLAY=:0; feh --bg-scale ' +
+            random.choice(select_files), shell=True)
 
     
 def main():
-    while 1:
-        randombg()
-#        global TIME
-        time.sleep(5)
+    randombg()
+
 
 if __name__ == '__main__':
     main()
-
-
-
